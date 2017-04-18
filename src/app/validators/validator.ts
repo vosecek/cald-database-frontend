470e6b24a174bfea04641d9ede859a29aa5b0ad4
Jiri Vosecek <jiri.vosecek@gmail.com>
2017-04-18 18:41:31 +0200

    Prazdny email je validni, nemusi se zadavat

--- src/app/validators/validator.ts
+++ src/app/validators/validator.ts
@@ -5,6 +5,8 @@ export class EmailValidator {
 	public static validate(c: AbstractControl) {
 		let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
+		if (c.value == null) return true;
+
 		return EMAIL_REGEXP.test(c.value) ? null : {
 			validateEmail: {
 				valid: false
