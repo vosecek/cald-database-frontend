8b5a2ddcd0429f51eba2e722e526972bf5859a82
Jiri Vosecek <jiri.vosecek@gmail.com>
2017-04-26 09:44:45 +0200

    Spravny datovy typ odpovedi pri prazdnem vstupu.

--- src/app/theme/validators/email.validator.ts
+++ src/app/theme/validators/email.validator.ts
@@ -1,16 +1,16 @@
-import {AbstractControl} from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 export class EmailValidator {
 
-  public static validate(c:AbstractControl) {
-    let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+	public static validate(c: AbstractControl) {
+		let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-	if (c.value.length == 0) return true;
+		if (c.value.length == 0) return null;
 
-    return EMAIL_REGEXP.test(c.value) ? null : {
-      validateEmail: {
-        valid: false
-      }
-    };
-  }
+		return EMAIL_REGEXP.test(c.value) ? null : {
+			validateEmail: {
+				valid: false
+			}
+		};
+	}
 }
--- src/app/validators/validator.ts
+++ src/app/validators/validator.ts
@@ -5,7 +5,7 @@ export class EmailValidator {
 	public static validate(c: AbstractControl) {
 		let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-		if (c.value == null) return true;
+		if (c.value == null) return null;
 
 		return EMAIL_REGEXP.test(c.value) ? null : {
 			validateEmail: {
